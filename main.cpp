9f0d06996850e5a1bbbd97cd4adaa46423983b32
Kasper123213 <radom.kasper@gmail.com>
2023-12-03 23:23:58 +0100

    zkopiowanie

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <windows.h>
+#include "Test.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    SetConsoleOutputCP(CP_UTF8);    //ustawianie polskich znaków
+
+
+    cout << "Autor: Kasper Radom 264023" << endl <<"Algorytm przeglądu zupełnego"<<endl<< endl;
+
+    //inicjowanie pamięci na włączenie testów
+    Test *test = new Test;
+
+    //zwolnienie pamięci po objekcie test i wywołanie jego destruktora
+    delete test;
+
+
 }
